2310acb67064cf9ffc68de699d93b85373b46fb5
Luna Duclos <luna.duclos@embark-studios.com>
2019-09-11 12:51:05 +0200

    QueryerContext: If the wrapped connection implements neither QueryerContext nor Queryer, exit early and do nothing

--- conn.go
+++ conn.go
@@ -187,6 +187,13 @@ func (c wrappedConn) Query(query string, args []driver.Value) (driver.Rows, erro
 }
 
 func (c wrappedConn) QueryContext(ctx context.Context, query string, args []driver.NamedValue) (rows driver.Rows, err error) {
+	// Quick skip path: If the wrapped connection implements neither queryercontext not queryer, we have absolutely nothing to do
+	_, hasQueryerContext := c.parent.(driver.QueryerContext)
+	_, hasQueryer := c.parent.(driver.Queryer)
+	if !hasQueryerContext && !hasQueryer {
+		return nil, driver.ErrSkip
+	}
+
 	if !c.hasOpExcluded(OpSQLConnQuery) {
 		span := c.GetSpan(ctx).NewChild(OpSQLConnQuery)
 		span.SetLabel("component", "database/sql")
