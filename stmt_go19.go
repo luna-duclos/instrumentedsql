40d03108b6f4dc6b3c67bac2c3670f61ae2ed449
Tiernan <neovortex@gmail.com>
2020-11-03 19:17:13 +1000

    Fixes issue #41 (NumInput() returns -1 with NamedValueChecker) (#42)
    * Fixes issue #41
    Do not implement driver.ColumnConverter on go1.9+ as the act of implementing this interface causes a side effect with a compatibility wrapper.
    Instead only implement the replacement driver.NamedValueChecker interface on go1.19+ and handle compatibility with drivers implementing driver.ColumnConverter ourselves to avoid the unintentional side effects from happening when driver.ColumnConverter is not implemented by the underlying driver.
    
    * Add license information for code copied from the Go standard library

--- stmt_go19.go
+++ stmt_go19.go
@@ -1,13 +1,109 @@
+// Copyright 2011 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE.golang file.
+
+// +build go1.9
+
 package instrumentedsql
 
-import "database/sql/driver"
+import (
+	"database/sql/driver"
+	"fmt"
+	"reflect"
+)
 
 var _ driver.NamedValueChecker = wrappedStmt{}
 
-func (c wrappedStmt) CheckNamedValue(v *driver.NamedValue) error {
-	if checker, ok := c.parent.(driver.NamedValueChecker); ok {
-		return checker.CheckNamedValue(v)
+func (s wrappedStmt) CheckNamedValue(v *driver.NamedValue) error {
+	if checker, ok := s.parent.(driver.NamedValueChecker); ok {
+		err := checker.CheckNamedValue(v)
+		if err != driver.ErrSkip {
+			return err
+		}
+	}
+
+	if converter, ok := s.parent.(driver.ColumnConverter); ok {
+		cc := ccChecker{
+			cci:  converter,
+			want: s.NumInput(),
+		}
+		return cc.CheckNamedValue(v)
 	}
 
 	return driver.ErrSkip
+}
+
+// ccChecker wraps the driver.ColumnConverter and allows it to be used
+// as if it were a NamedValueChecker. If the driver ColumnConverter
+// is not present then the NamedValueChecker will return driver.ErrSkip.
+type ccChecker struct {
+	cci  driver.ColumnConverter
+	want int
+}
+
+func (c ccChecker) CheckNamedValue(nv *driver.NamedValue) error {
+	if c.cci == nil {
+		return driver.ErrSkip
+	}
+	// The column converter shouldn't be called on any index
+	// it isn't expecting. The final error will be thrown
+	// in the argument converter loop.
+	index := nv.Ordinal - 1
+	if c.want <= index {
+		return nil
+	}
+
+	// First, see if the value itself knows how to convert
+	// itself to a driver type. For example, a NullString
+	// struct changing into a string or nil.
+	if vr, ok := nv.Value.(driver.Valuer); ok {
+		sv, err := callValuerValue(vr)
+		if err != nil {
+			return err
+		}
+		if !driver.IsValue(sv) {
+			return fmt.Errorf("non-subset type %T returned from Value", sv)
+		}
+		nv.Value = sv
+	}
+
+	// Second, ask the column to sanity check itself. For
+	// example, drivers might use this to make sure that
+	// an int64 values being inserted into a 16-bit
+	// integer field is in range (before getting
+	// truncated), or that a nil can't go into a NOT NULL
+	// column before going across the network to get the
+	// same error.
+	var err error
+	arg := nv.Value
+	nv.Value, err = c.cci.ColumnConverter(index).ConvertValue(arg)
+	if err != nil {
+		return err
+	}
+	if !driver.IsValue(nv.Value) {
+		return fmt.Errorf("driver ColumnConverter error converted %T to unsupported type %T", arg, nv.Value)
+	}
+	return nil
+}
+
+var valuerReflectType = reflect.TypeOf((*driver.Valuer)(nil)).Elem()
+
+// callValuerValue returns vr.Value(), with one exception:
+// If vr.Value is an auto-generated method on a pointer type and the
+// pointer is nil, it would panic at runtime in the panicwrap
+// method. Treat it like nil instead.
+// Issue 8415.
+//
+// This is so people can implement driver.Value on value types and
+// still use nil pointers to those types to mean nil/NULL, just like
+// string/*string.
+//
+// This function is mirrored in the database/sql/driver package.
+func callValuerValue(vr driver.Valuer) (v driver.Value, err error) {
+	if rv := reflect.ValueOf(vr); rv.Kind() == reflect.Ptr &&
+		rv.IsNil() &&
+		rv.Type().Elem().Implements(valuerReflectType) {
+		return nil, nil
+	}
+	return vr.Value()
 }
