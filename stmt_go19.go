e87bbc8afcd1a375f1ea0bda499f3c087a4b8d47
Tiernan Messmer <tiernan.messmer@jcu.edu.au>
2020-11-03 10:21:28 +1000

    Add license information for code copied from the Go standard library

--- stmt_go19.go
+++ stmt_go19.go
@@ -1,3 +1,7 @@
+// Copyright 2011 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE.golang file.
+
 // +build go1.9
 
 package instrumentedsql
