70888bdb983df36579595d47302ef1feb00be19a
Dimitrij Drus <dimitrij.drus@innoq.com>
2021-08-10 09:49:41 +0200

    better tag organization

--- tags.go
+++ tags.go
@@ -1,11 +1,16 @@
 package instrumentedsql
 
+// Standard tags for databases according to the opentracing specification
 const (
 	Component       = "component"
 	DBInstance      = "db.instance"
 	DBStatement     = "db.statement"
-	DBStatementArgs = "args"
 	DBType          = "db.type"
 	DBUser          = "db.user"
 	SpanKind        = "span.kind"
+)
+
+// Internal tags
+const (
+	DBStatementArgs = "args"
 )
